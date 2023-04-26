8851a2690ab039b14b03963e826f198ff12141e8
temirlan <tb@plazz.ag>
2023-04-26 21:19:07 +0200

    post request done successfully

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ import express from "express" // так мы импортируем express
 
 const app = express() // express равно app
 
+app.use(express.json()) // это нужно для того чтобы наше node.js приложение могло читать json запросы, без этого будет выходить undefined
+
 app.get("/", (req, res) => {
   res.send("Hello world")
 })  // сделали get запрос, который в качестве ответа отправляет Hello world
 
-app.listen(4444,(err) => {
+
+app.post("/auth/login", (req, res) => {
+    console.log(req.body)
+    res.json({
+        success: true,
+    })
+})
+
+
+
+
+app.listen(3000,(err) => {
     if(err) {
         return console.log(err)
     }
