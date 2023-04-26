a77f31093e96e8d7d894fdc858334cce421355f9
temirlan <tb@plazz.ag>
2023-04-26 21:47:16 +0200

    mongodb connected to node.js app

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+import { error } from "console"
 import express from "express" // так мы импортируем express
+import jwt from "jsonwebtoken" // нужен для того чтобы шифровать данные в токен
+import mongoose from "mongoose"
+
+ mongoose.connect(
+    
+    "mongodb+srv://baisalovt:123456qwertz@cluster0.7pl0ups.mongodb.net/?retryWrites=true&w=majority")   // так мы подключились к mongodb
+.then(() => console.log("DB ok"))
+.catch((err) => console.log("DB error", err))
 
 const app = express() // express равно app
 
@@ -10,9 +19,24 @@ app.get("/", (req, res) => {
 
 
 app.post("/auth/login", (req, res) => {
-    console.log(req.body)
+    console.log(req.body)  // просто чтобы посмотреть что находиться в body
+        
+
+    if(req.body.email === "test@gmail.com") {
+
+         // c помощью jwt.sign мы превратили данные в токен
+        const token = jwt.sign({
+            email: req.body.email,
+            fullname: "John Legend"
+        },
+        "secret123", // это ключь с помощью которого мы зашивровали наши данные которые находятся в токене
+        
+        )
+    }
+   
     res.json({
         success: true,
+        token,
     })
 })
 
