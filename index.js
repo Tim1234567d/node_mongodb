596999b1c45a092ad0d5b7035730847a31a80903
temirlan <tb@plazz.ag>
2023-05-04 15:34:33 +0200

    checkAuth file added

--- index.js
+++ index.js
@@ -1,52 +1,50 @@
-import express from "express" // так мы импортируем express
-import jwt from "jsonwebtoken" // нужен для того чтобы шифровать данные в токен
-import bcrypt from "bcrypt" // Нужно для того чтобы зашифровать пароль который приходит с frontend
-import mongoose from "mongoose" // нужен для того чтобы подключиться к mongodb, мы его специально скачали с помощью npm install mongoose
-import {registerValidation} from "./validations/auth.js" // мы импортировали нашу валидацию
-import {validationResult} from "express-validator"  // этот пакет будет проверять есть ли ошибки в веденных данных или нет
+import express from "express" 
+import jwt from "jsonwebtoken" 
+import bcrypt from "bcrypt" 
+import mongoose from "mongoose" 
+import {registerValidation} from "./validations/auth.js" 
+import {validationResult} from "express-validator" 
 import UserModel from "./models/User.js"
+import checkAuth from "./utils/checkAuth.js"
 
 
 
-// ***********************
+// ****************************************************** new part
  mongoose.connect(
-    "mongodb+srv://baisalovt:123456qwertz@cluster0.7pl0ups.mongodb.net/blog?retryWrites=true&w=majority")   // так мы подключились к mongodb
-.then(() => console.log("DB ok"))  // Если подключение прошло удачно то в консоле выводится "DB ok"
-.catch((err) => console.log("DB error", err)) // Если при подключении произошла какая-либо ошибка мы выводим ее в консоле
+    "mongodb+srv://baisalovt:123456qwertz@cluster0.7pl0ups.mongodb.net/blog?retryWrites=true&w=majority")  
+.then(() => console.log("DB ok"))  
+.catch((err) => console.log("DB error", err)) 
 
-const app = express() // express равно app
+const app = express() 
 
-app.use(express.json()) // это нужно для того чтобы наше node.js приложение могло читать json запросы, без этого будет выходить undefined
+app.use(express.json()) 
 
 
-
-// ***********************************
-
+// ****************************************************** new part
 app.post("/auth/register",registerValidation, async(req, res) => {
 
     try {
-        const errors = validationResult(req)  // мы взяли validationResult из пакета validationResult, этот пакет нужен для валидации
+        const errors = validationResult(req)  
     if(!errors.isEmpty()) {
         return res.status(400).json(errors.array())
-    } // Если есть ошибки то верни статус 400
-
+    } 
 
 // **************
-    const password = req.body.password; // так мы вытащили пароль с body
-    const salt = await bcrypt.genSalt(10) // Это алгоритм шифрования пароля, то есть каким образом должен шифроваться пароль
-    const hash = await bcrypt.hash(password, salt) // таким образом мы зашифровали пароль
+    const password = req.body.password; 
+    const salt = await bcrypt.genSalt(10) 
+    const hash = await bcrypt.hash(password, salt) 
 
     const doc = new UserModel({
         email: req.body.email, 
         fullName: req.body.fullName, 
         avatarUrl: req.body.avatarUrl, 
-        passwordHash: hash,  // это зашифрованный пароль
+        passwordHash: hash,  
 
     })
 
 
 // *************
-    const user = await doc.save();  // таким образом мы сохранили все данные 
+    const user = await doc.save();  
 
 // *************
     const token = jwt.sign({
@@ -54,19 +52,19 @@ app.post("/auth/register",registerValidation, async(req, res) => {
     },
     "secret123",
     {
-        expiresIn: "30d",  // столько дней будет жить токен
+        expiresIn: "30d",  
     }
     )
 
 
 // **********
 
-const {passwordHash, ...userData} = user._doc  // таким образом мы скрыли отображение passwordHash в консоле, иначе бы эта информация показывалась бы в консоле вместе с другими данными
+const {passwordHash, ...userData} = user._doc  
 // ***********
     res.json({
         ...userData,
         token,
-    }) // как ответ вернули все данные которые сохранили
+    }) 
 
 
 
@@ -76,14 +74,11 @@ const {passwordHash, ...userData} = user._doc  // таким образом мы
         res.status(500).json({
             message: "Не удалось зарегистрироваться",
         })
-    }
-
-    
-})
+    }    
+});
 
 
-// ********************************
-
+// ****************************************************** new part
 app.post("/auth/login", async(req, res) => {
     try {
         const user = await UserModel.findOne({email: req.body.email });
@@ -128,14 +123,38 @@ app.post("/auth/login", async(req, res) => {
             message: "Не удалось авторизоваться",
         })
     }
-})
+});
 
-// ****************************************
+
+
+// ****************************************************** new part
+app.get("/auth/me",checkAuth, async(req, res) => {
+    try {
+        const user = await UserModel.findById(req.userId);
+    
+        if (!user) {
+          return res.status(404).json({
+            message: 'Пользователь не найден',
+          });
+        }
+    
+        const { passwordHash, ...userData } = user._doc;
+    
+        res.json(userData);
+      }  catch (err) {
+        console.log(err);
+        res.status(500).json({
+          message: 'Нет доступа',
+        });
+      }
+});
+
+
+// ****************************************************** new part
 app.listen(3000,(err) => {
     if(err) {
         return console.log(err)
     }
-
     console.log("Server ok")
-} )  // Эта строка обязательно нужна, какой порт должен слушать наш node js приложение
+} )  
 
--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,22 +1,22 @@
 import jwt from 'jsonwebtoken';
 
 export default (req, res, next) => {
-  const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
+  const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');  // Таким образом мы удалили слова Bearer которое автоматически создается программой
 
   if (token) {
     try {
-      const decoded = jwt.verify(token, 'secret123');
+      const decoded = jwt.verify(token, 'secret123'); // таким образом мы расшифровали токен
 
       req.userId = decoded._id;
-      next();
+      next(); // Если удалось расшифровать токен, то следующую функцию
     } catch (e) {
       return res.status(403).json({
         message: 'Нет доступа',
-      });
+      }); // Если  мы не смогли расшифровать токен, то выведи сообщение 'Нет доступа'
     }
   } else {
     return res.status(403).json({
       message: 'Нет доступа',
-    });
+    }); // Если токена не будет то верни ответ 'Нет доступа'
   }
 };
